7ac9fab13e228e6162692a5306f943a0bfd9d311
vishalraj9102 <vishalrajmehra95@gmail.com>
2025-03-22 01:03:14 +0530

    Updated code

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,5 @@ from app.routes import stock_api
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(stock_api)  # Register routes
+    app.register_blueprint(stock_api, url_prefix="/api")  # Add URL Prefix
     return app
